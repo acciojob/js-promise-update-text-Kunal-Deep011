caba37e78ffee6d9be5fabb13132a15f47e29128
Kunal-Deep011 <117732649+Kunal-Deep011@users.noreply.github.com>
2023-03-31 13:00:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function updateOutput() {
+  // Create a Promise that resolves after 1 second with the string "Hello, world!"
+  const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Hello, world!');
+    }, 1000);
+  });
+
+  // When the Promise resolves, update the text of the HTML element with ID "output"
+  myPromise.then((result) => {
+    const outputElem = document.getElementById('output');
+    outputElem.innerText = result;
+  });
+}
+
+// Call the updateOutput() function to start the process
+updateOutput();
+
